0cae266e35fd3673123db684facd107a2ce238cd
Zabil C.M <zabilcm@gmail.com>
2010-02-24 17:38:44 +0530

    Migrated to testng

--- regression/src/test/java/org/openmrs/test/LoginPageTest.java
+++ regression/src/test/java/org/openmrs/test/LoginPageTest.java
@@ -2,23 +2,23 @@ package org.openmrs.test;
 
 import junit.framework.Assert;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.openmrs.page.HomePage;
 import org.openmrs.page.LoginPage;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class LoginPageTest {
 
 	private FirefoxDriver driver;
 
-	@Before
+        @BeforeTest
 	public void setUp() throws Exception {
 		driver = new FirefoxDriver();
 	}
 
-	@After
+	@AfterTest
 	public void tearDown() throws Exception {
 		driver.close();
 	}
